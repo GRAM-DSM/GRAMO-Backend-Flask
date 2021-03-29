95fe5b9c9003f8cf5c0916d9dd5fecbf215a348e
junkiYoon <juneki4933@gmail.com>
2021-03-29 17:08:35 +0900

    update app.run
    add ip 0.0.0.0

--- server/app.py
+++ server/app.py
@@ -4,4 +4,4 @@ from server import create_app
 if __name__ == "__main__":
     Base.metadata.create_all(engine)
     app = create_app()
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
