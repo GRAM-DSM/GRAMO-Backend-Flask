084d5cb23e1426eae4398e408308565bf0d539e9
junkiYoon <juneki4933@gmail.com>
2021-04-07 08:27:55 +0900

    update model init
    change engine, session options

--- server/model/__init__.py
+++ server/model/__init__.py
@@ -1,15 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, scoped_session
 import redis
 
 from server.config import MYSQL_DB_URL
 
-engine = create_engine(MYSQL_DB_URL)
+engine = create_engine(MYSQL_DB_URL, pool_recycle=3600)
 
 Base = declarative_base()
 
-Session = sessionmaker(bind=engine)
+Session = scoped_session(sessionmaker(bind=engine, autocommit=False, autoflush=False))
 session = Session()
 
 Redis = redis.StrictRedis(host='localhost', port=6379, db=0)
