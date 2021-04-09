fb588a446c32140f59edce031948137d2b79221d
junkiYoon <juneki4933@gmail.com>
2021-04-09 23:10:44 +0900

    create token expire time
    set access 1 day and refresh 1 week

--- server/__init__.py
+++ server/__init__.py
@@ -9,6 +9,8 @@ def create_app():
     _app = Flask(__name__)
 
     _app.secret_key = SECRET_KEY
+    _app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 86400
+    _app.config['JWT_REFRESH_TOKEN_EXPIRES'] = 604800
 
     _app.register_blueprint(bp)
 
