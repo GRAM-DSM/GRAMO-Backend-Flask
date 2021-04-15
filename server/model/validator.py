0fe40cc2dd9f8400cd0bd1d3728bcb1fe68287c1
junkiYoon <juneki4933@gmail.com>
2021-04-15 19:29:22 +0900

    update signup validator
    make choices in major

--- server/model/validator.py
+++ server/model/validator.py
@@ -6,7 +6,7 @@ class SignupValidator(Model):
     email = EmailType(serialized_name='email', required=True)
     password = StringType(serialized_name='password', required=True, min_length=5, max_length=20)
     name = StringType(serialized_name='name', required=True)
-    major = StringType(serialized_name='major', required=True)
+    major = StringType(serialized_name='major', required=True, choices=["ANDROID", "IOS", "BACKEND", "DESIGN"])
 
 
 class SendEmailCodeValidator(Model):
