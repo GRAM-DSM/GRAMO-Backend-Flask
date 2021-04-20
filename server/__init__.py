896f09dd654008f832068202727f2a7af8e2d2be
junkiYoon <juneki4933@gmail.com>
2021-04-20 16:51:28 +0900

    update token timeout
    timeout value in config

--- server/__init__.py
+++ server/__init__.py
@@ -2,15 +2,15 @@ from flask import Flask
 
 from server.router import bp
 
-from server.config import SECRET_KEY
+from server.config import SECRET_KEY, ACCESS_TIMEOUT, REFRESH_TIMEOUT
 
 
 def create_app():
     _app = Flask(__name__)
 
     _app.secret_key = SECRET_KEY
-    _app.config['JWT_ACCESS_TOKEN_EXPIRES'] = 86400
-    _app.config['JWT_REFRESH_TOKEN_EXPIRES'] = 604800
+    _app.config['JWT_ACCESS_TOKEN_EXPIRES'] = ACCESS_TIMEOUT
+    _app.config['JWT_REFRESH_TOKEN_EXPIRES'] = REFRESH_TIMEOUT
 
     _app.register_blueprint(bp)
 
