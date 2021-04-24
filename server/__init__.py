0e85eb81435fb2ac69a6d1492feb669d2d311581
junkiYoon <juneki4933@gmail.com>
2021-04-24 22:29:48 +0900

    create mysqldb init
    set mysqldb init app

--- server/__init__.py
+++ server/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 from server.router import bp
 
@@ -7,6 +8,9 @@ from server.config import SECRET_KEY, ACCESS_TIMEOUT, REFRESH_TIMEOUT
 
 def create_app():
     _app = Flask(__name__)
+
+    mysql = MySQL()
+    mysql.init_app(_app)
 
     _app.secret_key = SECRET_KEY
     _app.config['JWT_ACCESS_TOKEN_EXPIRES'] = ACCESS_TIMEOUT
