ce0bfc1bca74519778f6b91ef6c98e94a9651d76
junkiYoon <juneki4933@gmail.com>
2021-06-18 16:14:10 +0900

    delete create_all
    delete Base.metadata.create_all(Engine)

--- server/app.py
+++ server/app.py
@@ -1,10 +1,5 @@
-from flask_jwt_extended import JWTManager
-
-from server.model import Base, engine
 from server import create_app
 
 if __name__ == "__main__":
-    Base.metadata.create_all(engine)
     app = create_app()
-    jwt = JWTManager(app)
     app.run(host="0.0.0.0", debug=True)
